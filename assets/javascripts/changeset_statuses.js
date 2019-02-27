d88258008a905362d669142492401cae66c2f6a2
Alexandre D'Eschambeault <adeschambeault@exolnet.com>
2019-02-27 09:43:52 -0500

    Display combined status detail on a popover

--- assets/javascripts/changeset_statuses.js
+++ assets/javascripts/changeset_statuses.js
@@ -13,8 +13,49 @@ window.addEventListener('load', function() {
           return;
         }
 
-        revisionLink.classList.add('changeset-status');
-        revisionLink.classList.add('changeset-status--'+ data.state);
+        var details = document.createElement('details');
+
+        details.className = 'changeset-status__popover';
+        details.setAttribute('role', 'menu');
+
+        details.innerHTML = '<summary aria-haspopup="menu" class="changeset-status changeset-status--'+ data.state +'">•</summary>' +
+          '<div class="changeset-status__popup"><ul></ul></div>';
+
+        var ul = details.querySelector('ul');
+
+        data.statuses.forEach(function(status) {
+          var li = document.createElement('li');
+          li.innerHTML = '<span class="changeset-status changeset-status--'+ status.state +'">•</span> <strong></strong>'
+
+          var strong = li.querySelector('strong');
+
+          if (status.target_url) {
+            var a = document.createElement('a');
+
+            a.setAttribute('href', status.target_url);
+            a.setAttribute('target', '_blank');
+            a.setAttribute('rel', 'noopener noreferrer');
+
+            a.innerText = status.context;
+
+            strong.appendChild(a);
+          } else {
+            strong.innerText = status.context;
+          }
+
+          if (status.description) {
+            var em = document.createElement('em');
+
+            em.setAttribute('title', status.description);
+            em.innerText = status.description;
+
+            li.appendChild(em);
+          }
+
+          ul.appendChild(li);
+        });
+
+        revisionLink.parentNode.insertBefore(details, revisionLink);
       });
   }
 });
