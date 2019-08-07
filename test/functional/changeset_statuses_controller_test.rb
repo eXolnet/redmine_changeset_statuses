af77aa63e4740d0647120437a7caafc806697d1c
Alexandre D'Eschambeault <adeschambeault@exolnet.com>
2019-08-07 12:09:41 -0400

    Add test methods helper to run tests on Redmine 3 and 4

--- test/functional/changeset_statuses_controller_test.rb
+++ test/functional/changeset_statuses_controller_test.rb
@@ -17,7 +17,7 @@ class ChangesetStatusesControllerTest < ActionController::TestCase
 
   def test_create_changeset_statuses_on_default_repository_with_minimal_parameters
     assert_difference 'ChangesetStatus.count' do
-      post :create, :project_id => 'ecookbook', :rev => '1', :format => 'json', :context => "continuous-integration/jenkins", :state => "success"
+      compatible_request :post, :create, :project_id => 'ecookbook', :rev => '1', :format => 'json', :context => "continuous-integration/jenkins", :state => "success"
 
       assert_response :success
     end
@@ -28,7 +28,7 @@ class ChangesetStatusesControllerTest < ActionController::TestCase
 
   def test_create_changeset_statuses_on_default_repository_with_all_parameters
     assert_difference 'ChangesetStatus.count' do
-      post :create,
+      compatible_request :post, :create,
         :project_id => 'ecookbook',
         :rev => '1',
         :format => 'json',
@@ -45,85 +45,85 @@ class ChangesetStatusesControllerTest < ActionController::TestCase
   end
 
   def test_create_changeset_statuses_on_default_repository_on_non_api_requests
-    post :create, :project_id => 'ecookbook', :rev => '1', :state => "success"
+    compatible_request :post, :create, :project_id => 'ecookbook', :rev => '1', :state => "success"
 
     assert_response 403
   end
 
   def test_create_changeset_statuses_on_invalid_project
-    post :create, :project_id => 'invalid', :rev => '1', :format => 'json', :state => "success"
+    compatible_request :post, :create, :project_id => 'invalid', :rev => '1', :format => 'json', :state => "success"
 
     assert_response 404
   end
 
   def test_create_changeset_statuses_on_invalid_repository
-    post :create, :project_id => 'ecookbook', :repository_id => 'invalid', :format => 'json', :rev => '1', :state => "success"
+    compatible_request :post, :create, :project_id => 'ecookbook', :repository_id => 'invalid', :format => 'json', :rev => '1', :state => "success"
 
     assert_response 404
   end
 
   def test_create_changeset_statuses_on_invalid_rev
-    post :create, :project_id => 'ecookbook', :rev => 'invalid', :format => 'json', :state => "success"
+    compatible_request :post, :create, :project_id => 'ecookbook', :rev => 'invalid', :format => 'json', :state => "success"
 
     assert_response 404
   end
 
   def test_show_changeset_statuses_on_default_repository
-    get :show, :project_id => 'ecookbook', :rev => '1', :format => 'json'
+    compatible_request :get, :show, :project_id => 'ecookbook', :rev => '1', :format => 'json'
 
     assert_response :success
   end
 
   def test_show_changeset_statuses_on_non_api_requests
-    get :show, :project_id => 'invalid', :rev => '1'
+    compatible_request :get, :show, :project_id => 'invalid', :rev => '1'
 
     assert_response 403
   end
 
   def test_show_changeset_statuses_on_invalid_project
-    get :show, :project_id => 'invalid', :rev => '1', :format => 'json'
+    compatible_request :get, :show, :project_id => 'invalid', :rev => '1', :format => 'json'
 
     assert_response 404
   end
 
   def test_show_changeset_statuses_on_invalid_repository
-    get :show, :project_id => 'ecookbook', :repository_id => 'invalid', :rev => '1', :format => 'json'
+    compatible_request :get, :show, :project_id => 'ecookbook', :repository_id => 'invalid', :rev => '1', :format => 'json'
 
     assert_response 404
   end
 
   def test_show_changeset_statuses_on_invalid_rev
-    get :show, :project_id => 'ecookbook', :rev => 'invalid', :format => 'json'
+    compatible_request :get, :show, :project_id => 'ecookbook', :rev => 'invalid', :format => 'json'
 
     assert_response 404
   end
 
   def test_show_combined_changeset_statuses_on_default_repository
-    get :show_combined, :project_id => 'ecookbook', :rev => '1', :format => 'json'
+    compatible_request :get, :show_combined, :project_id => 'ecookbook', :rev => '1', :format => 'json'
 
     assert_response :success
   end
 
   def test_show_combined_changeset_statuses_on_non_api_requests
-    get :show_combined, :project_id => 'invalid', :rev => '1'
+    compatible_request :get, :show_combined, :project_id => 'invalid', :rev => '1'
 
     assert_response 403
   end
 
   def test_show_combined_changeset_statuses_on_invalid_project
-    get :show_combined, :project_id => 'invalid', :rev => '1', :format => 'json'
+    compatible_request :get, :show_combined, :project_id => 'invalid', :rev => '1', :format => 'json'
 
     assert_response 404
   end
 
   def test_show_combined_changeset_statuses_on_invalid_repository
-    get :show_combined, :project_id => 'ecookbook', :repository_id => 'invalid', :rev => '1', :format => 'json'
+    compatible_request :get, :show_combined, :project_id => 'ecookbook', :repository_id => 'invalid', :rev => '1', :format => 'json'
 
     assert_response 404
   end
 
   def test_show_combined_changeset_statuses_on_invalid_rev
-    get :show_combined, :project_id => 'ecookbook', :rev => 'invalid', :format => 'json'
+    compatible_request :get, :show_combined, :project_id => 'ecookbook', :rev => 'invalid', :format => 'json'
 
     assert_response 404
   end
