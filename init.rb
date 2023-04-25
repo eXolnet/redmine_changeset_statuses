1adbae1000cb25f99e028b7e07361d0e415ce716
Alexandre D’Eschambeault <adeschambeault@exolnet.com>
2023-04-25 15:55:55 -0400

    build: bump version to v1.3.0

--- init.rb
+++ init.rb
@@ -1,6 +1,6 @@
 require 'redmine'
 
-CHANGESET_STATUSES_VERSION = '1.2.2'
+CHANGESET_STATUSES_VERSION = '1.3.0'
 
 Redmine::Plugin.register :redmine_changeset_statuses do
   name 'Changeset Statuses'
